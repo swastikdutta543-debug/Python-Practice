101fb20a3c499611cf5044f172fe3af4278ef776
Swastik_chem_engineer <swastikdutta543@gmail.com>
2026-05-13 23:29:18 +0530

    thuis is the total which i have learnt on 13/05/2026

---
+++ calculator.py
@@ -0,0 +1,7 @@
+x = int(input("enter the value of x: "))
+y = int(input("enter the value of y: "))
+
+# addition
+
+
+print(x+y)
--- helloword.py
+++ helloword.py
@@ -1,3 +1,26 @@
 print("hello world")
+
+#ask user for their name
 a = input("what's your name? ")
-print("hello",a)
+
+# Remove White space from str
+a = a.strip()
+
+#capitalize user's name
+a= a.capitalize() # it only capitalize the first letter of the whole sentence not every 1st letter of every word
+
+a = a.title() # in this case every 1st letter of every word of a sentence will be capitalized
+
+#**** we can also add the both like a = a.strip().title() and also can be done like this a = input("....").strip().title()
+
+#say hello to user
+print("hello1 " + a) 
+print("hello2",a)
+print("hello3 ", end='')
+print(a)
+print(f"hello4 {a}")
+print("hello5", a, sep='???') # output--> hello???a
+
+#split the user name
+first,last = a.split(" ")
+print(f"hello {first}")
